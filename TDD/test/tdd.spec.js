88092dbb63e9c39c5569392da7518c38fcda64c2
dspizzirri-meli <damian.spizzirri@grupoesfera.com.ar>
2019-07-29 17:51:08 -0300

    carpeta TDD, mocha y chai

---
+++ TDD/test/tdd.spec.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+//const assert = require('chai').expect;
+//const assert = require('chai').should;
+
+describe('Prueba', ()=>{
+    it('console.log', ()=>{
+        console.log(`Hola Mundo`);
+    });
+});
