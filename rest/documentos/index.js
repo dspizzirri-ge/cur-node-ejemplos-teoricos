4c0b89d37a52ddce37759404c7f71e53eee23775
dspizzirri-meli <damian.spizzirri@grupoesfera.com.ar>
2019-08-08 12:19:21 -0300

    manejor de errores y correcciones en servicios rest

--- rest/documentos/index.js
+++ rest/documentos/index.js
@@ -3,21 +3,33 @@ function consultar(dbb, equipo){
     try{
        if(equipo)
           return dbb.find({selector: {$or:[{ local: { $eq: equipo }}, { visitante: { $eq: equipo }}]}});
-       return dbb.allDocs();
-    
+       return dbb.allDocs();    
     }catch(e){
         throw Error(e);
     }
 }
 
-function crear(db, data){
-    
-    if(Array.isArray(data))
-        return db.bulkDocs(data);
-    return db.post(data);
+function crear(dbb, data){
+
+    try{
+        if(Array.isArray(data))
+            return dbb.bulkDocs(data);
+        return dbb.post(data);
+    }catch(e){
+        throw Error(e);
+    }
+}
+
+function eliminar(dbb, doc){
+    try{
+        dbb.remove(doc);
+    }catch(e){
+        throw Error(e);
+    } 
 }
 
 module.exports = {
     consultar,
-    crear
+    crear,
+    eliminar
 }
--- rest/index.js
+++ rest/index.js
@@ -3,8 +3,8 @@ const PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-find'));
 
 const express = require('express');
-const bodyParser = require('body-parser')
-const app = express()
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,90 +13,96 @@ const port = 3000;
 const db = new PouchDB('torneo2019');
 
 app.get('/resultados/', 
-    (req, res) => {
+    (req, res, next) => {
         try{
             documentos.consultar(db)
             .then((arrayDoc) => res.json(arrayDoc))
             .catch(err => { res.status(500); res.json({ error: err })});
         }catch(e){
-            res.status(500); 
-            res.json({ error: e.message })
+            next(e);
         }
     });
 
 app.get('/resultados/:equipo/', 
-    (req, res) => {
+    (req, res, next) => {
         try{
             documentos.consultar(db, req.params.equipo)
             .then((arrayDoc) => {res.json(arrayDoc.docs)})
             .catch(err => { res.status(500); res.json({ error: err })});
         }catch(e){
-            res.status(500); 
-            res.json({ error: e.message })
+            next(e);
         }
     });
 
 app.post('/resultados/', 
-    (req, res) => {
+    (req, res, next) => {
         try{
             documentos.crear(db, req.body)
             .then(doc => res.json(doc))
             .catch(err => { res.status(500); res.json({ error: err })});
 
         }catch(e){
-            res.status(500); 
-            res.json({ error: e.message })
+            next(e);
         }
     });
 
 app.delete('/resultados/', 
-(req, res) => {
+(req, res, next) => {
     try{
         db.destroy()
         .then((response) => res.json(response))
         .catch(err => { res.status(500); res.json({ error: err });});
     }catch(e){
-        res.status(500); 
-        res.json({ error: e.message })
+        next(e);
     }
 });
 
 app.delete('/resultados/:equipo/local', 
-(req, res) => {
+(req, res, next) => {
     try{
-        db.find({ selector: { local: { eq: req.params.equipo } } })
-        .then(doc => db.remove(doc))
+        documentos.consultar(db, req.params.equipo)
+        .then(doc => documentos.eliminar(db, doc))
         .then(doc => res.json(doc))
         .catch(err => { res.status(500); res.json({ error: err });});
     }catch(e){
-        res.status(500); 
-        res.json({ error: e.message })
+        next(e);
     }
 });
 
 app.delete('/resultados/:id/', 
-    (req, res) => {
+    (req, res, next) => {
         try{    
             db.get(req.params.id)
             .then(doc => db.remove(doc))
             .then(doc => res.json(doc))
             .catch(err => { res.status(500); res.json({ error: err });});
         }catch(e){
-            res.status(500); 
-            res.json({ error: e.message })
+            next(e);
         }
     });
 
 app.put('/resultado/', 
-    (req, res) => {
+    (req, res, next) => {
         try{
             db.put(req.body)
             .then(doc => res.json(doc))
             .catch(err => { res.status(500); res.json({ error: err });});
         }catch(e){
-            res.status(500); 
-            res.json({ error: e.message })
+            next(e);
         }
     });
 
-app.listen(port, () => console.log(`Escuchando en el puerto ${port}!`))
+app.use((err, req, res, next)=>{
+    console.log(`Manejando error: ${err}`);
+    res.status(500); 
+    res.json({ error: err.message });
+});    
+
+app.listen(port, () => {
+
+    console.log(`Escuchando en el puerto ${port}!`);
+    console.log(`_Documentos_`);
+    db.info()
+        .then((info)=> console.log(info))
+        .catch((e)=> console.log(e));
+});
