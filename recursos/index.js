465cd27156a0209aba9565a749e6d63539a3f3d0
dspizzirri-meli <damian.spizzirri@grupoesfera.com.ar>
2019-08-09 16:25:42 -0300

    middleware y recursos

---
+++ recursos/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/public`));
+app.use(morgan('tiny'));
+
+app.get('/:carpeta/:tipo/:archivo', (req, res, next)=>{
+    res.sendFile(`${__dirname}/${req.params.carpeta}/${req.params.tipo}/${req.params.archivo}`);
+});
+
+app.post('/guardardatos', (req, res, next)=>{
+     res.sendFile(`${__dirname}/public/guardado.html`);
+});
+
+app.listen(3000, ()=>console.log(`__dirname: ${__dirname}`));
