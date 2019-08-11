15b1c82afdd4fece99a6e6a1ea42ab197757df77
Damian <damianspizzirri@gmail.com>
2019-08-11 15:22:46 -0300

    chaiHttp pruebas

--- TDD/src/tdd.js
+++ TDD/src/tdd.js
@@ -7,8 +7,42 @@ const puerto = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next)=>{
+app.get('/', (req, res, next) => {
     res.json({});
+});
+
+app.get('/materia/:materia/:curso', (req, res, next) => {
+
+    const materia = req.params.materia;
+    const curso = req.params.curso;
+    const horasSemanales = 8;
+    const profesorTitular = "Profesor1";
+    const alumnos = [
+        { nombre: "alumno1", apellido: "apellido1", dni: "22.222.222" },
+        { nombre: "alumno2", apellido: "apellido2", dni: "22.222.226" },
+        { nombre: "alumno3", apellido: "apellido3", dni: "22.222.225" },
+        { nombre: "alumno4", apellido: "apellido4", dni: "22.222.224" },
+        { nombre: "alumno5", apellido: "apellido5", dni: "22.222.223" }
+    ];
+
+    res.json({ materia, curso, horasSemanales, profesorTitular, alumnos });
+});
+
+app.post('/materia/:materia/:curso', (req, res, next) => {
+
+    const materia = req.params.materia;
+    const curso = req.params.curso;
+    const horasSemanales = 8;
+    const profesorTitular = "Profesor1";
+    const alumnos = [
+        { nombre: "alumno1", apellido: "apellido1", dni: "22.222.222" },
+        { nombre: "alumno2", apellido: "apellido2", dni: "22.222.226" },
+        { nombre: "alumno3", apellido: "apellido3", dni: "22.222.225" },
+        { nombre: "alumno4", apellido: "apellido4", dni: "22.222.224" },
+        { nombre: "alumno5", apellido: "apellido5", dni: "22.222.223" }
+    ];
+
+    res.json({ materia, curso, horasSemanales, profesorTitular, alumnos });
 });
 
 app.listen(puerto, () => {
--- TDD/test/tdd.spec.js
+++ TDD/test/tdd.spec.js
@@ -3,18 +3,32 @@ const chaiHttp = require('chai-http');
 const { app } = require('../src/tdd');
 
 chai.use(chaiHttp);
-chai.should();
+let expect = chai.expect;
 
-describe('Pruebas HTTP', ()=>{
-    describe('GET /', ()=>{
-        it('deberia obtener todos los alumnos', (done)=>{
-           chai.request(app)
-            .get('/')
-            .end((err, res)=>{
-                res.should.have.status(200);
-                res.body.should.be.a({});
-                done();
-            });
+describe('Pruebas HTTP', () => {
+    describe('GET /', () => {
+        it('No deberia obtener datos', (done) => {
+            chai.request(app)
+                .get('/')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(JSON.stringify(res.body)).to.equal("{}");
+                    done();
+                });
+        });
+
+        it("Deberia obtener los alumnos de la materia 'Algebra' de la catedra '2190'", (done) => {
+            chai.request(app)
+                .get('/materia/algebra/2190')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('materia').to.be.equal('algebra');
+                    expect(res.body).to.have.property('curso').to.be.equal('2190');
+                    expect(res.body).to.have.property('horasSemanales').to.not.be.NaN;
+                    expect(res.body).to.have.property('horasSemanales').to.be.least(1);
+                    expect(res.body).to.have.property('profesorTitular').to.not.be.null;
+                    done();
+                });
         });
     });
 });
