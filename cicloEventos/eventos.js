ffba9456977ca20cfc529287df530c7f634cebcf
dspizzirri-meli <damian.spizzirri@grupoesfera.com.ar>
2019-08-14 12:01:32 -0300

    Ejemplo ciclo de eventos

--- cicloEventos/eventos.js
+++ cicloEventos/eventos.js
@@ -1,3 +1,11 @@
+console.log("Primer mensaje");
+
+setTimeout(()=>console.log("Segundo mensaje"), 3000);
+setTimeout(()=>console.log("Tercer mensaje"), 0);
+
+console.log("Cuarto mensaje");
+
+
 for (let i = 0; i < 10; i++) {
 
     console.log(`Vuelta: ${i}`);
